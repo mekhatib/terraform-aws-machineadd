11dd3af65119347b6c335ba5dfc7c45a7ac075c2
mekhatib <melkhatib@ziggo.nl>
2025-06-02 20:44:26 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,16 +8,6 @@ output "instance_private_ips" {
   value       = module.ec2_instances.private_ips
 }
 
-output "instance_elastic_ips" {
-  description = "Elastic IPs of the EC2 instances (if assigned)"
-  value       = module.ec2_instances.elastic_ips
-}
-
-output "instance_names" {
-  description = "Names of the EC2 instances"
-  value       = module.ec2_instances.instance_names
-}
-
 output "key_pair_name" {
   description = "Name of the created key pair"
   value       = aws_key_pair.main.key_name
@@ -27,4 +17,13 @@ output "private_key_pem" {
   description = "Private key in PEM format (sensitive)"
   value       = tls_private_key.main.private_key_pem
   sensitive   = true
+}
+
+# Additional outputs if needed
+output "instance_details" {
+  description = "Map of instance names to their IDs"
+  value = {
+    for i, id in module.ec2_instances.instance_ids : 
+      "${var.project_name}-${var.environment}-instance-${i + 1}" => id
+  }
 }
