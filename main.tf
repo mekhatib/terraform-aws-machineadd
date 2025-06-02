64169b6b4519f9e5fb469603e5ba480638c8bfd0
mekhatib <melkhatib@ziggo.nl>
2025-06-02 21:33:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,33 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Data source to fetch infrastructure module's remote state
+data "terraform_remote_state" "infrastructure" {
+  backend = "remote"
+  
+  config = {
+    organization = var.tfc_organization
+    workspaces = {
+      name = var.infrastructure_workspace_name
+    }
+  }
+}
+
+# Local values to simplify access to infrastructure outputs
+locals {
+  # Infrastructure outputs
+  vpc_id                = data.terraform_remote_state.infrastructure.outputs.vpc_id
+  vpc_cidr_block        = data.terraform_remote_state.infrastructure.outputs.vpc_cidr_block
+  private_subnet_ids    = data.terraform_remote_state.infrastructure.outputs.private_subnet_ids
+  public_subnet_ids     = data.terraform_remote_state.infrastructure.outputs.public_subnet_ids
+  all_subnet_ids        = data.terraform_remote_state.infrastructure.outputs.all_subnet_ids
+  app_security_group_id = data.terraform_remote_state.infrastructure.outputs.app_security_group_id
+  instance_role_name    = data.terraform_remote_state.infrastructure.outputs.instance_role_name
+  common_tags          = data.terraform_remote_state.infrastructure.outputs.common_tags
+  environment          = data.terraform_remote_state.infrastructure.outputs.environment
+  project_name         = data.terraform_remote_state.infrastructure.outputs.project_name
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
 
@@ -38,13 +65,13 @@ resource "tls_private_key" "main" {
 }
 
 resource "aws_key_pair" "main" {
-  key_name   = "${var.project_name}-${var.environment}-keypair"
+  key_name   = "${local.project_name}-${local.environment}-keypair"
   public_key = tls_private_key.main.public_key_openssh
   
   tags = merge(
-    var.common_tags,
+    local.common_tags,
     {
-      Name = "${var.project_name}-${var.environment}-keypair"
+      Name = "${local.project_name}-${local.environment}-keypair"
     }
   )
 }
@@ -54,27 +81,27 @@ module "ec2_instances" {
   source  = "app.terraform.io/test-khatib/ec2-instances/aws"
   version = "~> 0.0"
   
-  environment  = var.environment
-  project_name = var.project_name
+  environment  = local.environment
+  project_name = local.project_name
   
   instances = [
     {
-      name               = "${var.project_name}-${var.environment}-instance-1"
+      name               = "${local.project_name}-${local.environment}-instance-1"
       instance_type      = var.instance_types["flavor1"]
-      subnet_id          = length(var.private_subnet_ids) > 0 ? var.private_subnet_ids[0] : var.all_subnet_ids[0]
-      security_group_ids = [var.app_security_group_id]
-      iam_role_name      = var.instance_role_name
+      subnet_id          = length(local.private_subnet_ids) > 0 ? local.private_subnet_ids[0] : local.all_subnet_ids[0]
+      security_group_ids = [local.app_security_group_id]
+      iam_role_name      = local.instance_role_name
       user_data_file     = null
-      tags               = merge(var.common_tags, { Name = "${var.project_name}-${var.environment}-instance-1" })
+      tags               = merge(local.common_tags, { Name = "${local.project_name}-${local.environment}-instance-1" })
     },
     {
-      name               = "${var.project_name}-${var.environment}-instance-2"
+      name               = "${local.project_name}-${local.environment}-instance-2"
       instance_type      = var.instance_types["flavor2"]
-      subnet_id          = length(var.private_subnet_ids) > 1 ? var.private_subnet_ids[1] : (length(var.all_subnet_ids) > 1 ? var.all_subnet_ids[1] : var.all_subnet_ids[0])
-      security_group_ids = [var.app_security_group_id]
-      iam_role_name      = var.instance_role_name
+      subnet_id          = length(local.private_subnet_ids) > 1 ? local.private_subnet_ids[1] : (length(local.all_subnet_ids) > 1 ? local.all_subnet_ids[1] : local.all_subnet_ids[0])
+      security_group_ids = [local.app_security_group_id]
+      iam_role_name      = local.instance_role_name
       user_data_file     = null
-      tags               = merge(var.common_tags, { Name = "${var.project_name}-${var.environment}-instance-2" })
+      tags               = merge(local.common_tags, { Name = "${local.project_name}-${local.environment}-instance-2" })
     }
   ]
   
@@ -85,11 +112,11 @@ module "ec2_instances" {
   elastic_ip_allocation_ids = []
   create_dns_records        = false
   route53_zone_id          = null
-  route53_zone_name        = "${var.project_name}-${var.environment}.internal"
+  route53_zone_name        = "${local.project_name}-${local.environment}.internal"
   enable_monitoring        = var.enable_monitoring
   cpu_alarm_threshold      = var.cpu_alarm_threshold
   alarm_actions           = []
-  create_instance_profiles = true
+  create_instance_profiles = false
   additional_volumes      = []
   kms_key_id              = null
   
